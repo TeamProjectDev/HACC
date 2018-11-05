311aee26c4b255ea851f49339b4d54a23b7826fd
Alex <eieghtbitdolphin@gmail.com>
2018-11-04 15:17:25 -1000

    final direct push to master; will make new branch

--- magic.js
+++ magic.js
@@ -5,6 +5,14 @@ function GetImage(image) {
     return `<img src='images/${image}' />`
 }
 
+function getScrollPercent() {
+    var h = document.documentElement, 
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+    return console.log((h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100);
+}
+
 Vue.component('download', {
     props: ['text','link'],
     template: ` <a class="button" :href="link"> {{text}} </a> ` 
@@ -19,6 +27,7 @@ var app = new Vue({
             team: 'The Team'
         },
         title: {
+            showCaseImage: 'images/plants/plant1.png',
             name: 'Project Grass',
             logo: GetImage('Logo_temp.png'),
             type: 'Plant Identification Software'
@@ -29,26 +38,73 @@ var app = new Vue({
                 {image: 'images/plants/plant2.jpg', text:'smells like woomy spirit'},
                 {image: 'images/plants/plant3.jpg', text:'Get those Salmon Eggs BOI!'}
             ],
-            arrow: GetImage('pointers.png'),
             currentCounter: 0,
+            leftArrow: '<',
+            rightArrow: '>'
         },
         main: {
             slogan: 'Plant Idetification Made Easy',
             sloganDetail: 'Discover Thousands of Plants With Advanced AI',
             downloadText: 'Download Now',
             downloadLink: 'http://Google.com'
+        },
+        discover: {
+            slogan: 'Discover A Expanding Community',
+            recent: 'Recent Discoveries around Oahu',
+
         }
     },
     methods: {
         prevImage: function() {
-            this.showcase.currentCounter == 0 ? this.showcase.currentCounter = this.showcase.images.length-1 : this.showcase.currentCounter--;
-            console.log(this.showcase.currentCounter);
+            this.showcase.currentCounter == 0 ? this.showcase.currentCounter = this.showcase.images.length-1 : 
+            this.showcase.currentCounter--;
         },
         nextImage: function() {
-            this.showcase.currentCounter == this.showcase.images.length-1 ? this.showcase.currentCounter = 0 : this.showcase.currentCounter++;
-            console.log(this.showcase.currentCounter);
+            this.showcase.currentCounter == this.showcase.images.length-1 ? this.showcase.currentCounter = 0 : 
+            this.showcase.currentCounter++;
         },
+        uploads: function() {
+            return api.uploaded
+        },
+        template: function(discoverIn,discoveredDate,discoveredBy,plantDisovered,description) {
+            return `
+            <div class="cardHolder">
+                <div class="rootHolder>
+                    <div class="imageHolder"> 
+                    
+                    </div>
+        
+                    <div class="textHolder>
+                    
+                        <div class="discoveredIn"> 
+                            <h3>Discovered In</h3>
+                            <p>${discoverIn}</p>
+                        </div>
+        
+                        <div class="discoveredDate">
+                            <h3> Date </h3>
+                            <p> ${discoveredDate} </p>
+                        </div>
+        
+                        <div class = "discoveredBy">
+                            <h3>Discovered By</h3>
+                            <p>${discoveredBy}</p>
+                        </div>
+        
+                        <div class = "discoveredPlant">
+                            <h3>Plant Discovered</h3>
+                            <p>${plantDisovered}</p>
+                        </div>
+        
+                        <div class = "description">
+                            <h3>Description</h3>
+                            <p>${description}</p>
+                        </div>
+        
+                    </div>
+                </div>
+            </div>
+            `
+        }
     }
 });
-
-console.log(app.showcase.currentImage);
---
+++ templateAPI.js
@@ -0,0 +1,15 @@
+const api = {
+    data: {
+        maxHold : 10,
+        example : true
+    },
+    uploaded: [
+        {
+            user : 'Jeraldy Cascayan',
+            discoveryLocation : 'Waikiki,HI',
+            discoveryDate : '18/11/04',
+            discoveredPlant : 'Hawaiian Baby WoodRose / Argyreia nervosa',
+            description : '10/10 ; A trip to remember'
+        }
+    ]
+}
